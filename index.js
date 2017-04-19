33bb24b419d2ed0ad03476890cf5fb819fc1f8e4
Andrew Betts <andrew@trib.tv>
2017-04-19 14:44:46 -0700

    Use node bindings for bsdiff

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const exec = require('child-process-promise').exec;
 const isValidURL = require('valid-url').isUri;
 const mkDir = require('mkdirp-promise');
 const fetch = require('node-fetch');
+const bsdiff = require('node-bsdiff').diff;
 
 const DEFAULT_TTL = 3600;
 const MAX_SOURCE_SIZE = 10 * 1024 * 1024;
@@ -27,12 +28,8 @@ exports.compareURLs = function compareURLs (req, res) {
       if (!(param in req.query)) throw new Error('`' + param + '` is a required query parameter');
       if (!isValidURL(req.query[param])) throw new Error('Value supplied in `' + param + '` does not look like a valid URL');
 
-      console.log('Starting download of ' + req.query[param]);
-
       return fetch(req.query[param])
         .then(resp => {
-
-          console.log('Receiving ' + req.query[param]);
 
           if (resp.headers.get('content-length') > MAX_SOURCE_SIZE) {
             throw new Error('Source size too large.  Maximum source size is ' + MAX_SOURCE_SIZE + ' bytes');
@@ -43,11 +40,10 @@ exports.compareURLs = function compareURLs (req, res) {
           meta.name = url.parse(req.query[param]).pathname.replace(/^.*\/([^\/]+)\/?$/, '$1');
           meta.path = path.join(paths[param], meta.name);
 
-          const isCompressed = resp.headers.get('Content-Encoding') === 'gzip' || meta.name.match(/\.(tgz|gz|gzip)$/);
-          const pipe = isCompressed ?  resp.body.pipe(zlib.createGunzip()) : resp.body;
+          const isCompressed = Boolean(resp.headers.get('Content-Encoding') === 'gzip' || meta.name.match(/\.(tgz|gz|gzip)$/));
+          const respStream = isCompressed ?  resp.body.pipe(zlib.createGunzip()) : resp.body;
 
-          console.log('Streaming to '+meta.path, isCompressed);
-          pipe.pipe(fs.createWriteStream(meta.path));
+          console.log('Receiving ' + req.query[param], isCompressed ? "compressed":"", resp.headers.get('content-length'));
 
           if (resp.headers.get('cache-control').indexOf('max-age') !== -1) {
             meta.ttl = Number.parseInt(resp.headers.get('cache-control').replace(/^.*max-age=(\d+).*?$/, '$1'), 10);
@@ -61,25 +57,30 @@ exports.compareURLs = function compareURLs (req, res) {
 
           meta.surrogateKey = resp.headers['surrogate-key'] || null;
 
-          return new Promise(resolve => resp.body.on('finish', resolve.bind(null, meta)));
+          const bufs = [];
+          respStream.on('data', data => bufs.push(data));
+          return new Promise(resolve => {
+            respStream.on('finish', () => {
+              meta.content = Buffer.concat(bufs);
+              resolve(meta);
+            });
+          });
         })
       ;
     })))
 
     // Create patch and serve it
     .then(([from, to]) => {
-      console.log('Computing patch...');
-      return exec('./bsdiff ' + from.path + ' ' + to.path + ' ' + paths.patch)
-        .then(() => {
-          res.status(200);
-          res.set('Cache-Control', 'max-age=' + Math.min(from.ttl, to.ttl));
-          if (from.surrogateKey || to.surrogateKey) {
-            res.set('Surrogate-Key', from.surrogateKey + ' ' + to.surrogateKey);
-          }
-          console.log("Sending patch file...");
-          res.sendFile(path.resolve(paths.patch));
-        })
-      ;
+      console.log('Computing patch of ' + from.name + ' (' + from.content.length + ') and ' + to.name + ' (' + to.content.length + ')');
+      const patch = bsdiff(from.content, to.content);
+      res.status(200);
+      res.set('Cache-Control', 'max-age=' + Math.min(from.ttl, to.ttl));
+      res.set('Content-Type', 'application/octet-stream')
+      if (from.surrogateKey || to.surrogateKey) {
+        res.set('Surrogate-Key', from.surrogateKey + ' ' + to.surrogateKey);
+      }
+      console.log("Sending patch file (" + patch.length + ")...");
+      res.send(patch);
     })
 
     // In error cases, just redirect so the client downloads the destination file in full
